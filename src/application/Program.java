aa01b5cd862cf72459699f1b5293d68f24017bfd
cleberdasilva <cleber.supertux@gmailcom>
2023-02-27 17:51:55 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,10 +16,18 @@ public class Program {
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		System.out.println("");
 		
 		System.out.println("=== TEST 2: seller findByDeparment ===");
 		Department deparment = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(deparment);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		System.out.println("");
+		
+		System.out.println("=== TEST 3: seller findByAll ===");
+		list = sellerDao.findAll(); // como estou reaproveitando a variavel da lista, não preciso declarar novamente
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
